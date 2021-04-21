87016b5d727b4f7655b088e75a1a27c7aa22e664
Marcus Bernales <marcus@totum.io>
2021-04-21 12:39:50 -0700

    Found all the media!

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import * as fs from "fs";
-import * as path from "path";
 import glob from "glob";
 import logger from "./logger";
 
@@ -9,18 +7,34 @@ const INPUT_DIRECTORY = "/Volumes/photo/raws";
 
 type DirectoryPath = string;
 type MediaPath = string;
+type Filetype = string;
+
+const SUPPORTED_PHOTOS = ["jpg", "png", "arw", "dng"];
+const SUPPORTED_VIDEOS = ["mov", "mp4"];
+const SUPPORTED_FILETYPES = SUPPORTED_PHOTOS.concat(SUPPORTED_VIDEOS);
+
+function createSupportedFileGlob(filetypes: Filetype[]): string {
+  const allcaps = filetypes.map((type) => type.toUpperCase());
+  return filetypes.concat(allcaps).join("|");
+}
 
 async function findCompatibleMedia(
   pathToSearch: DirectoryPath
 ): Promise<MediaPath[]> {
   return new Promise((resolve, reject) => {
-    glob(`${pathToSearch}/**/*.dng`, {}, function (err, files) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(files);
+    const supportedFileGlob = createSupportedFileGlob(SUPPORTED_FILETYPES);
+    logger.info("looking for", { supportedFileGlob });
+    glob(
+      `${pathToSearch}/**/*.@(${supportedFileGlob})`,
+      {},
+      function (err, files) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(files);
+        }
       }
-    });
+    );
   });
 }
 
